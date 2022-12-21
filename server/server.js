5ef6dc71771c6eb42487edcd34c851a0e3bd958b
vantyse <nonsokingsley61@gmail.com>
2022-12-21 10:46:55 +0100

    socket cors origin update

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const {Server} = require('socket.io');
 
 const io = new Server(server, {
     cors : {
-        origin: `http://localhost:3000`,
+        origins: [`https://vanchat.up.railway.app/`, `http://localhost:3000`],
         methods: ['GET', 'POST']
     }
 })
